87e7293ee523a56858b575e391ed81695cfd892f
aesthetech-create <57689668+aesthetech-create@users.noreply.github.com>
2023-01-26 14:12:20 -0500

    removed the factor form and reverted it to "a,b" form

--- rk4c-sliders.py
+++ rk4c-sliders.py
@@ -13,7 +13,7 @@ y0 = 0     # where y(x0) = y0
 z0 = 0     # where z(x0) = z0
 dx = 0.1   # step size
 x_end = 30 # final accepted value for x before calculation terminates
-factor = 1 # THIS IS A/B!!!!!
+a, b = 1 # THIS IS A/B!!!!!
 
 # Initial variables, and building the array to place calculated values of every iteration of RK4 of both equations
 x = x0
@@ -24,25 +24,25 @@ y_values = [y0]
 z_values = [x0] 
 
 # derivative functions definition
-def dydx(x, y, z, factor): # dy/dx, where y and z are functions of x
-    return x + math.pow(0.01, y)*y
+def dydx(x, y, z, a, b): # dy/dx, where y and z are functions of x
+    return 
 
-def dzdx(x, y, z, factor): # dz/dx, where y and z are functions of x
-    return 47 - x + (math.pow(0.02, z)*dydx(x, y, z, factor))
+def dzdx(x, y, z, a, b): # dz/dx, where y and z are functions of x
+    return 47 - x + (math.pow(0.02, z)*dydx(x, y, z, a, b))
 
 # main RK4 functiom
-def RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, factor):
+def RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, a, b):
     
     # calculating RK4 variables as before, but this time alternating between the two equations
     # k values are for RK4 of dydx, and m values are for RK4 of dzdx
-    k1 = dx * dydx(x, y, z, factor) 
-    m1 = dx * dzdx(x, y, z, factor)
-    k2 = dx * dydx(x + dx/2., y + k1/2., z + m1/2., factor)
-    m2 = dx * dzdx(x + dx/2., y + k1/2., z + m1/2., factor)
-    k3 = dx * dydx(x + dx/2., y + k2/2., z + m2/2., factor)
-    m3 = dx * dzdx(x + dx/2., y + k2/2., z + m2/2., factor)
-    k4 = dx * dydx(x + dx, y + k3, z + m3, factor)
-    m4 = dx * dzdx(x + dx, y + k3, z + m3, factor)
+    k1 = dx * dydx(x, y, z, a, b) 
+    m1 = dx * dzdx(x, y, z, a, b)
+    k2 = dx * dydx(x + dx/2., y + k1/2., z + m1/2., a, b)
+    m2 = dx * dzdx(x + dx/2., y + k1/2., z + m1/2., a, b)
+    k3 = dx * dydx(x + dx/2., y + k2/2., z + m2/2., a, b)
+    m3 = dx * dzdx(x + dx/2., y + k2/2., z + m2/2., a, b)
+    k4 = dx * dydx(x + dx, y + k3, z + m3, a, b)
+    m4 = dx * dzdx(x + dx, y + k3, z + m3, a, b)
 
     # summative calculation
     y = y + 1./6.*(k1+2*k2+2*k3+k4)
@@ -53,7 +53,7 @@ def RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, factor):
 while x <= x_end: # while we haven't reached intended final calculation area (x)
     
     # perform one iteration of the RK4
-    x, y, z = RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, factor)
+    x, y, z = RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, a, b)
     
     #append outputted values into the value lists so they can be plotted by matplotlib later
     x_values.append(x)
@@ -87,7 +87,7 @@ ab_slider = Slider(
 def update(val):
     while x <= x_end: # while we haven't reached intended final calculation area (x)
         # perform one iteration of the RK4
-        x, y, z = RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, factor)
+        x, y, z = RungeKuttaCoupled(x, y, z, dx, dydx, dzdx, a, b)
         
         #append outputted values into the value lists so they can be plotted by matplotlib later
         x_values.append(x)
